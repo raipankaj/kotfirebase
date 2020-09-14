ac7f8a60f9dc39c1bc7d0a2ab2c047ec58debdb2
Pankaj <pankaj.rai16@gmail.com>
2020-09-14 21:35:28 +0530

    Updated firebase extensions

--- kotfirebase/src/main/java/com/source/kotfirebase/FirebaseExtension.kt
+++ kotfirebase/src/main/java/com/source/kotfirebase/FirebaseExtension.kt
@@ -2,17 +2,16 @@
 package com.source.kotfirebase
 
 import android.content.Context
-import android.graphics.Bitmap
 import android.os.Bundle
 import com.source.kotfirebase.abs.analytics.Analytics
-import com.source.kotfirebase.abs.firestore.Firestore
-import com.source.kotfirebase.abs.storage.Storage
-fun String.getFirebaseCollection(sync: Boolean = false) = Firestore.getFromCollection(this, sync)
+import com.source.kotfirebase.abs.firestore.KotFirestore
 
-fun String.getFirebaseDocuments(sync: Boolean = false) = Firestore.getFromDocument(this, sync)
+fun String.getFirebaseCollection(sync: Boolean = false) = KotFirestore.getFromCollection(this, sync)
+
+fun String.getFirebaseDocuments(sync: Boolean = false) = KotFirestore.getFromDocument(this, sync)
 
 inline fun <reified T> String.getFirebaseDocumentsIn(sync: Boolean = false) =
-    Firestore.getDocumentResultsIn<T>(this, sync)
+    KotFirestore.getDocumentResultsIn<T>(this, sync)
 
 fun Context.logBundledEvent(name: String, analyticsFunc: Bundle.() -> Unit) {
     val bundle = Bundle()
